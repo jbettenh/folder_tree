f32fcadf612c4344c69ba372ba3bf9fb03e42e47
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-06-01 14:45:21 +0200

    Initial commit

---
+++ folder_tree.py
@@ -0,0 +1,25 @@
+#! python 3
+# This will list out all folders and subfolders for a given directory.
+
+import argparse
+import os
+
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Parser')
+    parser.add_argument('--path', type=dir_path, help='This is the path to the start directory.')
+
+
+def main():
+    args = arguments()
+
+
+if __name__ == '__main__':
+    main()
