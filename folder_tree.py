1f0a2f19a6398cbcacb6228a82e0bb28524cc997
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-06-05 19:18:22 +0200

    lib docx to add hyperlinks

--- folder_tree.py
+++ folder_tree.py
@@ -3,6 +3,8 @@
 
 import argparse
 import os
+import docx
+# from docx.enum.dml import MSO_THEME_COLOR_INDEX
 
 
 def dir_path(string):
@@ -18,14 +20,48 @@ def arguments():
 
     return parser.parse_args()
 
+def add_hyperlink(paragraph, text, url):
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element and a new w:rPr element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    # Create a new Run object and add the hyperlink into it
+    r = paragraph.add_run ()
+    r._r.append (hyperlink)
+
+    # A workaround for the lack of a hyperlink style (doesn't go purple after using the link)
+    # Delete this if using a template that has the hyperlink style in it
+  #  r.font.color.theme_color = MSO_THEME_COLOR_INDEX.HYPERLINK
+   # r.font.underline = True
+
+    return hyperlink
+
+
 
 def main():
     args = arguments()
 
     for dirpath, dir, files in os.walk(args.path):
         path = dirpath.split('/')
-        print(dirpath)
+        print(dir, dirpath)
 
+    document = docx.Document()
+    p = document.add_paragraph('Folder: ')
+    add_hyperlink(p, 'folder name', dirpath)
+    document.save('demo_hyperlink.docx')
 
 if __name__ == '__main__':
     main()
