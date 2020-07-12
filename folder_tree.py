6184ee2a1015a9b014dfb88975e227fc6ffa67c8
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-07-12 11:04:51 +0200

    stackoverflow print with dir option

--- folder_tree.py
+++ folder_tree.py
@@ -3,6 +3,7 @@
 
 import argparse
 from pathlib import Path
+from itertools import islice
 import docx
 from docx.enum.dml import MSO_THEME_COLOR_INDEX
 
@@ -14,27 +15,46 @@ tee =    '├── '
 last =   '└── '
 
 
-def tree(dir_path: Path, prefix: str=''):
-    """A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
-    """
-    contents = list(dir_path.iterdir())
-    # contents each get pointers that are ├── with a final └── :
-    pointers = [tee] * (len(contents) - 1) + [last]
-    for pointer, path in zip(pointers, contents):
-        yield prefix + pointer + path.name
-        if path.is_dir(): # extend the prefix and recurse:
-            extension = branch if pointer == tee else space
-            # i.e. space because last, └── , above so no more |
-            yield from tree(path, prefix=prefix+extension)
-
-
 def arguments():
     parser = argparse.ArgumentParser(description='Parser')
     parser.add_argument('--path', type=Path,default=Path(__file__).absolute().parent / "Users", help='This is the path to the folder')
 
     return parser.parse_args()
+
+
+def tree(dir_path: Path, level: int = -1, limit_to_directories: bool = False, length_limit: int = 1000):
+        """Given a directory Path object print a visual tree structure"""
+        dir_path = Path(dir_path)  # accept string coerceable to Path
+        files = 0
+        directories = 0
+
+        def inner(dir_path: Path, prefix: str = '', level=-1):
+            nonlocal files, directories
+            if not level:
+                return  # 0, stop iterating
+            if limit_to_directories:
+                contents = [d for d in dir_path.iterdir() if d.is_dir()]
+            else:
+                contents = list(dir_path.iterdir())
+            pointers = [tee] * (len(contents) - 1) + [last]
+            for pointer, path in zip(pointers, contents):
+                if path.is_dir():
+                    yield prefix + pointer + path.name
+                    directories += 1
+                    extension = branch if pointer == tee else space
+                    yield from inner(path, prefix=prefix + extension, level=level - 1)
+                elif not limit_to_directories:
+                    yield prefix + pointer + path.name
+                    files += 1
+
+        print(dir_path.name)
+        iterator = inner(dir_path, level=level)
+        for line in islice(iterator, length_limit):
+            print(line)
+        if next(iterator, None):
+            print(f'... length_limit, {length_limit}, reached, counted:')
+        print(f'\n{directories} directories' + (f', {files} files' if files else ''))
+
 
 def add_hyperlink(paragraph, text, url):
     # This gets access to the document.xml.rels file and gets a new relation id value
@@ -69,7 +89,7 @@ def add_hyperlink(paragraph, text, url):
 def main():
     args = arguments()
 
-    for line in tree(args.path/ 'Documents'):
+    for line in tree(args.path/ 'Documents', limit_to_directories=True):
         print(line)
 
    # for dirpath, dirnames, files in os.walk(args.path):
@@ -82,6 +102,7 @@ def main():
     p = document.add_paragraph('Folder: ')
     add_hyperlink(p, 'folder name', dirpath)
     document.save('test/demo_hyperlink.docx')
+
 
 if __name__ == '__main__':
     main()
